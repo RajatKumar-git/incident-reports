3ff6358d70e9060efdfb8e599e14f8c0fb6eb632
RajatKumar-git <62398114+RajatKumar-git@users.noreply.github.com>
2026-09-24 19:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ incidentId = st.text_input('Enter Incident Id')
 
 if st.button("submit"):
   st.write("Below is the Incident Details")
-  if incidentId = 'INC000112':
+  if incidentId == 'INC000112':
     st.write('Low Priority')
     priority = 'P3'
     issueRaisedOn = datetime.now()
     created_date = issueRaisedOn - timedelta(days=2)
     ttl = 'Within Two Days'
-  elif incidentId = 'INC000123':
+  elif incidentId == 'INC000123':
     st.write('Medium Priority')
     priority = 'P2'   
     issueRaisedOn = datetime.now()
     created_date = issueRaisedOn - timedelta(days=1)
     ttl = 'Within One Day'
-  elif incidentId = 'INC000122':
+  elif incidentId == 'INC000122':
     st.write('High Priority')
     priority = 'P1'
     issueRaisedOn = datetime.now()
     created_date = issueRaisedOn - timedelta(days=1)
     ttl = 'Within One Hour'
-  elif incidentId = 'INC000111':
+  elif incidentId == 'INC000111':
     st.write('Medium Priority')
     priority = 'P2'
     issueRaisedOn = datetime.now()
